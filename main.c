ac5c226970df1eaf237812d6dff08e0ed774d060
Karolis Tarasauskas <karolis.tarasauskas@gmail.com>
2014-08-04 23:56:45 +0300

    minimum main file

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "png_lib.h"
+
 int main(int argc, char **argv) {
-    printf("Hello World!");
+
+	void *buff;
+	int height, width, depth;
+
+	PNG_decode("png.png", &buff, &height, &width, &depth);
+
     return 0;
 }
