8614d2d6779f5041f9c26f2db59776353f857dab
Karolis Tarasauskas <karolis.tarasauskas@gmail.com>
2014-08-09 22:40:42 +0300

    Added basic pixed extractor

--- main.c
+++ main.c
@@ -3,12 +3,98 @@
 
 #include "png_lib.h"
 
+struct img_params{
+	int height;
+	int width;
+	int bdepth;
+};
+
+struct img_params img;
+
+
+int extract_pixel(void *data, struct img_params, int x_coord, int y_coord);
+
+
 int main(int argc, char **argv) {
 
-	void *buff;
+	void *buff = NULL;
 	int height, width, depth;
+	int ret;
 
-	PNG_decode("png.png", &buff, &height, &width, &depth);
+	ret = PNG_decode("basn0g08.png", &buff, &height, &width, &depth);
+
+	if(ret == 0){
+		printf("decode complete\n");
+		printf("height = %d\nwidth = %d\ndepth = %d\n",
+			height,
+			width,
+			depth);
+		//print our image:
+		img.height = height;
+		img.bdepth = depth;
+		img.width = width;
+
+		int x_iter, y_iter;
+		for(y_iter = 0; y_iter<height; y_iter++){
+			for(x_iter = 0; x_iter<width; x_iter++){
+				printf("%d ", extract_pixel(buff, img, x_iter, y_iter));
+			}
+			printf("\n");
+		}
+		printf("\n");
+	};
+
 
     return 0;
 }
+
+int extract_pixel(void* data, struct img_params img, int x_coord, int y_coord)
+{
+	int ret_val;
+	int off_per_line;
+	int off;
+	uint8_t *start_b;
+	uint8_t bit_p;
+	//by png specs, we're allowed 1,2,4,8,16 bdepths.
+	off_per_line = img.width * img.bdepth / 8;
+
+	off = x_coord*img.bdepth/8 + y_coord * off_per_line;
+	start_b = ((uint8_t *)data)+off;
+
+	switch(img.bdepth){
+		case(1):
+			bit_p  = 8 - (x_coord % 8);
+			ret_val = *start_b & (1<<bit_p);
+			ret_val = ret_val >> bit_p;
+			break;
+		case(2):
+			bit_p = 6 - 2*(x_coord % 4);
+			ret_val = *start_b & (3<<bit_p);
+			ret_val = ret_val >> bit_p;
+			//apply two bit bitmask
+			ret_val &= 3;
+			break;
+		case(4):
+			bit_p = 4 - 4*(x_coord % 2);
+			ret_val = *start_b & (15<<bit_p);
+			ret_val = ret_val >> bit_p;
+			ret_val &= 15;
+			break;
+		case(8):
+			//just return every value
+			ret_val = *start_b;
+			break;
+		case(16):
+			//ret_val will be 2B long.
+			ret_val = *start_b;
+			start_b ++;
+			ret_val = (ret_val<<8) + *start_b;
+			break;
+		default:
+			printf("Invalid\n");
+			return -1;
+	};
+
+	return ret_val;
+// 	return x_coord*img.bdepth/8 + y_coord * off_per_line;
+};
